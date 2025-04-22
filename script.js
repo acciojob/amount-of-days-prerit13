a61a2c7d247d2fa1e7684959abf57a02586593bc
prerit13 <147128014+prerit13@users.noreply.github.com>
2025-04-22 23:50:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function daysOfAYear(year) {
+  // Leap year rule:
+  // - Divisible by 4 ➝ Leap Year
+  // - Except if divisible by 100 ➝ Not Leap Year
+  // - Unless also divisible by 400 ➝ Leap Year
+  if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+    return 366;
+  } else {
+    return 365;
+  }
+}
